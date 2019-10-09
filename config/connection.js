7549901244f30a5506662d500c301415b6ae033a
Mike Kravtsov <mike4040@me.com>
2019-10-09 15:37:03 -0700

    added remote DB connection

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,14 @@
 const mysql = require('mysql');
 
-const connection = mysql.createConnection({
+const localSQL = {
   host: 'localhost',
   port: 3306,
   user: 'root',
   password: 'password',
   database: 'burger_db'
-});
+};
+
+const connection = mysql.createConnection(process.env.JAWSDB_URL || localSQL);
 
 connection.connect(err => {
   if (err) return console.error('error connecting: ' + err.stack);
