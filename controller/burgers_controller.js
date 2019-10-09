82fe8fb1ca56751b56c4dd1a5098f119cdcbcecc
Mike Kravtsov <mike4040@me.com>
2019-10-08 18:26:06 -0700

    Controller ready

---
+++ controller/burgers_controller.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const router = express.Router();
+
+const burger = require('../models/burger');
+
+router.get('/', function(req, res) {
+  burger.all(function(data) {
+    const hbsObject = {
+      burgers: data
+    };
+    console.log(hbsObject);
+    res.render('index', hbsObject);
+  });
+});
+
+router.post('/api/burgers', function(req, res) {
+  burger.create('burger_name', req.body.name, result => res.json({ id: result.insertId }));
+});
+
+router.put('/api/burgers/:id', function(req, res) {
+  burger.update('devoured', true, req.params.id, function(result) {
+    if (result.changedRows == 0) res.status(404).end();
+    else res.status(200).end();
+  });
+});
+
+// Export routes for server.js to use.
+module.exports = router;
--- instructions.md
+++ instructions.md
@@ -128,16 +128,16 @@ Having an active and healthy commit history on GitHub is important for your futu
 
 #### Controller setup
 
-1. Inside your `burger` directory, create a folder named `controllers`.
+1. + Inside your `burger` directory, create a folder named `controllers`.
 
-2. In `controllers`, create the `burgers_controller.js` file.
+2. + In `controllers`, create the `burgers_controller.js` file.
 
-3. Inside the `burgers_controller.js` file, import the following:
+3. + Inside the `burgers_controller.js` file, import the following:
 
    * Express
    * `burger.js`
 
-4. Create the `router` for the app, and export the `router` at the end of your file.
+4. + Create the `router` for the app, and export the `router` at the end of your file.
 
 #### View setup
 
